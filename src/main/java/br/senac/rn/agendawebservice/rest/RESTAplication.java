c40f82ee72bda5febeac9368dc2389b00e347a1d
a9130439 <a9130439@02LAB108PC14.rn.senac.br>
2017-11-03 21:34:00 -0300

    continuacao 5

---
+++ src/main/java/br/senac/rn/agendawebservice/rest/RESTAplication.java
@@ -0,0 +1,10 @@
+package br.senac.rn.agendawebservice.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/resouces")
+public class RESTAplication extends Application {
+    
+}
